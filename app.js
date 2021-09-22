9bdd975108d8bbac7e9b02fdf89ac5d9c419b4af
Rahil Patel <patelrahil27@gmail.com>
2021-09-23 00:54:50 +0530

    Included ejs view templated and created layout structure

--- app.js
+++ app.js
@@ -2,9 +2,30 @@ const express = require('express');
 const app = express()
 const port = 3000
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use('/', express.static('public'));
+
+// Include routes.
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+    res.render('index');
+});
+
+app.get('/about-me', (req, res) => {
+    res.render('about-me');
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects');
+});
+
+app.get('/services', (req, res) => {
+    res.render('services');
+});
+
+app.get('/contact-me', (req, res) => {
+    res.render('contact-me');
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
