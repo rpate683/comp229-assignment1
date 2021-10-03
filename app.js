c9734fc480ac90ef9811b58baca2d6a24361f7ba
rahil Patel <rahil.patel.hs@gmail.com>
2021-10-02 22:53:09 -0400

    Code refactoring and formatting and integrated express generator

--- app.js
+++ app.js
@@ -1,37 +1,55 @@
-const express = require('express');
-const app = express()
-const port = 3000
+/**
+ * name: main.css
+ * Student ID: 301166451
+ * Student Name: Rahilkumar Patel
+ * Date: 02-10-2021
+ */
 
-// set the view engine to ejs
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var aboutMeRouter = require('./routes/about-me');
+var projectsRouter = require('./routes/projects');
+var servicesRouter = require('./routes/services');
+var contactMeRouter = require('./routes/contact-me');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use('/', express.static('public'));
 
-// Include routes.
-app.get('/', (req, res) => {
-    nav = 'index';
-    res.render('index');
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/about-me', aboutMeRouter);
+app.use('/projects', projectsRouter);
+app.use('/services', servicesRouter);
+app.use('/contact-me', contactMeRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
 });
 
-app.get('/about-me', (req, res) => {
-    nav = 'about-me';
-    res.render('about-me');
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
-app.get('/projects', (req, res) => {
-    nav = 'projects';
-    res.render('projects');
-});
-
-app.get('/services', (req, res) => {
-    nav = 'services';
-    res.render('services');
-});
-
-app.get('/contact-me', (req, res) => {
-    nav = 'contact-me';
-    res.render('contact-me');
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+module.exports = app;
---
+++ routes/contact-me.js
@@ -0,0 +1,14 @@
+/**
+ * name: main.css
+ * Student ID: 301166451
+ * Student Name: Rahilkumar Patel
+ * Date: 02-10-2021
+ */
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('contact-me', { nav: 'contact-me' });
+});
+
+module.exports = router;
