6c471d53571105aec0f1ecfd068efa001443ca34
rahil Patel <rahil.patel.hs@gmail.com>
2021-10-02 20:00:00 -0400

    final theme integration and design changes

--- app.js
+++ app.js
@@ -8,22 +8,27 @@ app.use('/', express.static('public'));
 
 // Include routes.
 app.get('/', (req, res) => {
+    nav = 'index';
     res.render('index');
 });
 
 app.get('/about-me', (req, res) => {
+    nav = 'about-me';
     res.render('about-me');
 });
 
 app.get('/projects', (req, res) => {
+    nav = 'projects';
     res.render('projects');
 });
 
 app.get('/services', (req, res) => {
+    nav = 'services';
     res.render('services');
 });
 
 app.get('/contact-me', (req, res) => {
+    nav = 'contact-me';
     res.render('contact-me');
 });
 
