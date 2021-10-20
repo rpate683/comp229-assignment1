9bb4bbdfc1a98477a4fe6c8618e8674803b5f8f1
rahil Patel <rahil.patel.hs@gmail.com>
2021-10-20 17:27:01 -0400

    Removed registration related functionalities

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -44,53 +44,53 @@ module.exports.authenticate = (req, res, next) => {
 }
 
 // GET Display register form.
-module.exports.register = (req, res, next) => {
-    // load login only if the user is not already logged
-    if (!req.user) {
-        return res.render('register', {
-            nav: 'login',
-            messages: [],
-            user: req.session.passport
-        });
-    }
-
-    return res.redirect('/');
-}
+// module.exports.register = (req, res, next) => {
+//     // load login only if the user is not already logged
+//     if (!req.user) {
+//         return res.render('register', {
+//             nav: 'login',
+//             messages: [],
+//             user: req.session.passport
+//         });
+//     }
+//
+//     return res.redirect('/');
+// }
 
 // POST register form and save user.
-module.exports.saveUser = (req, res, next) => {
-    // instantiate a user object
-    const newUser = new User({
-        username: req.body.username,
-        //password: req.body.password
-        email: req.body.email,
-        displayName: req.body.displayName,
-        user: req.session.passport
-    });
-
-    User.register(newUser, req.body.password, (err) => {
-        if (err) {
-            console.log("Error: Inserting New User");
-            console.log(err);
-            let errorMesage = '';
-            if (err.name === "UserExistsError") {
-                console.log('Error: User Already Exists!')
-                errorMesage = 'User Already Exists!';
-            }
-
-            return res.render('auth/register', {
-                title: 'Register',
-                messages: ['Error: Inserting New User!', errorMesage],
-                user: req.session.passport
-            });
-        } else {
-            // if no error exists, then registration is successful
-            return passport.authenticate('local')(req, res, () => {
-                res.redirect('/business-contacts');
-            });
-        }
-    });
-}
+// module.exports.saveUser = (req, res, next) => {
+//     // instantiate a user object
+//     const newUser = new User({
+//         username: req.body.username,
+//         //password: req.body.password
+//         email: req.body.email,
+//         displayName: req.body.displayName,
+//         user: req.session.passport
+//     });
+//
+//     User.register(newUser, req.body.password, (err) => {
+//         if (err) {
+//             console.log("Error: Inserting New User");
+//             console.log(err);
+//             let errorMesage = '';
+//             if (err.name === "UserExistsError") {
+//                 console.log('Error: User Already Exists!')
+//                 errorMesage = 'User Already Exists!';
+//             }
+//
+//             return res.render('auth/register', {
+//                 title: 'Register',
+//                 messages: ['Error: Inserting New User!', errorMesage],
+//                 user: req.session.passport
+//             });
+//         } else {
+//             // if no error exists, then registration is successful
+//             return passport.authenticate('local')(req, res, () => {
+//                 res.redirect('/business-contacts');
+//             });
+//         }
+//     });
+// }
 
 // GET logout user.
 module.exports.logout = (req, res, next) => {
